bea17ce0b3affcff03a2a2096197989ef7200091
Socheat <github@socheat.dev>
2026-03-09 20:00:15 +0700

    Refactor ci/cd workflows

--- .github/scripts/generate-build-matrix.sh
+++ .github/scripts/generate-build-matrix.sh
@@ -4,52 +4,36 @@ set -euo pipefail
 # This script detects files changed in a pull request compared to the main branch.
 # It looks for Dockerfiles among the changed files and generates a build matrix accordingly.
 # Example:
-# consul/1.22.0/Dockerfile
-# vault/1.10.0/Dockerfile
-
-GITHUB_BASE_REF=${GITHUB_BASE_REF:-main}
-GITHUB_HEAD_REF=${GITHUB_HEAD_REF:-}
+# library/{version}/Dockerfile
+TARGET=${TARGET:-${1:-**}}
 GITHUB_OUTPUT=${GITHUB_OUTPUT:-/dev/null}
 
 RUNNER_TEMP=${RUNNER_TEMP:-$(pwd)}
 BUILD_MATRIX_MANIFEST=$(mktemp -p "${RUNNER_TEMP}")
-
-if [ -z "$GITHUB_HEAD_REF" ]; then
-  echo "This script should be run in the context of a pull request."
-  exit 1
-fi
-
-TEMPLATES_CHANGED=false
+trap 'rm -f "$BUILD_MATRIX_MANIFEST"' EXIT
 
 echo "File changed:"
-for file in $(git diff "origin/${GITHUB_BASE_REF}" "HEAD" --name-only); do
+# shellcheck disable=SC2231
+for file in ${TARGET}/**/Dockerfile; do
 	echo "- ${file}"
-	if [[ "${file}" == *"/base/Dockerfile" ]]; then
-		TEMPLATES_CHANGED=true
-	fi
 	if [[ "${file}" == *"/.empty" ]] || [[ "${file}" == *"/Dockerfile" ]]; then
 		# Extract target and version from the file path
 		target=$(echo "${file}" | cut -d'/' -f1)
 		version=$(echo "${file}" | cut -d'/' -f2)
+		if [[ "${version}" == "base" ]]; then
+			echo "- Skipping ${version} change for ${target} as it is a base image and not a specific version."
+			continue
+		fi
+		
 		# Add to build matrix
 		echo "{\"target\":\"${target}\",\"version\":\"${version}\"}" >> "$BUILD_MATRIX_MANIFEST"
 	fi
 done
 
-# If any template Dockerfile has changed, we need to run the full build matrix generation script to ensure all relevant targets are included.
-if [ "$TEMPLATES_CHANGED" = true ]; then
-	echo "::warning::The template files have changed. Running the full build matrix generation script to ensure all relevant targets are included."
-	"$(dirname "$0")"/generate-build-matrix.sh
-	exit 0
-fi
-
 # Build JSON array and write to GITHUB_OUTPUT, quoting to prevent word splitting.
 echo "Generating build matrix..."
-cat "$BUILD_MATRIX_MANIFEST" | sort | uniq | jq -s '.'
+cat "$BUILD_MATRIX_MANIFEST" | sort -r | uniq | jq -s '.'
 
 # Set the output variable for GitHub Actions
 matrix_json=$(cat "$BUILD_MATRIX_MANIFEST" | sort | uniq | jq -sc '.')
 echo "matrix=${matrix_json}" >> "$GITHUB_OUTPUT"
-
-# Clean up
-rm -f "$BUILD_MATRIX_MANIFEST" || true

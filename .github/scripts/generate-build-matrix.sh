00ce873669b425b517104f4fd4888cd555107621
Socheat <github@socheat.dev>
2026-03-10 10:56:54 +0700

    Integrate `.github/scripts/generate-build-matrix.sh` with `.github/workflows/build.yml` workflow

--- .github/scripts/generate-build-matrix.sh
+++ .github/scripts/generate-build-matrix.sh
@@ -7,6 +7,9 @@ function github.log() {
 	fi
 }
 
+INPUT_TARGET=${INPUT_TARGET:-}
+INPUT_VERSION=${INPUT_VERSION:-}
+
 # This script detects files changed in a pull request compared to the main branch.
 # It looks for Dockerfiles among the changed files and generates a build matrix accordingly.
 # Example:
@@ -23,30 +26,39 @@ else
 	BUILD_MATRIX_MANIFEST=${_BUILD_MATRIX_MANIFEST}
 fi
 
-LIBRARIES=${LIBRARIES:-${@}}
-if [[ -z "${LIBRARIES}" ]]; then
-	# shellcheck disable=SC2231
-	LIBRARIES=$(ls **/docker-bake.hcl | cut -d/ -f1)
-fi
+if [[ -n "${INPUT_TARGET}" && -n "${INPUT_VERSION}" ]]; then
+	# If both target and version are provided, use them directly to generate the build matrix.
+	github.log "Using provided target and version: ${INPUT_TARGET}, ${INPUT_VERSION}"
+	echo "{\"target\":\"${INPUT_TARGET}\",\"version\":\"${INPUT_VERSION}\"}" >> "$BUILD_MATRIX_MANIFEST"
+else
+	# If target and version are not provided,
+	# detect changed files and generate the build matrix based on the file paths.
+	# If no specific libraries are provided as arguments, it will look for all libraries in the repository.
+	LIBRARIES=${LIBRARIES:-${@}}
+	if [[ -z "${LIBRARIES}" ]]; then
+		# shellcheck disable=SC2231
+		LIBRARIES=$(ls **/docker-bake.hcl | cut -d/ -f1)
+	fi
 
-github.log "File changed:"
-# shellcheck disable=SC2231
-for lib in ${LIBRARIES}; do
-	for file in ${lib}/**/Dockerfile; do
-		github.log "- ${file}"
-		if [[ "${file}" == *"/.empty" ]] || [[ "${file}" == *"/Dockerfile" ]]; then
-			# Extract target and version from the file path
-			target=$(echo "${file}" | cut -d'/' -f1)
-			version=$(echo "${file}" | cut -d'/' -f2)
-			if [[ "${version}" == "base" ]]; then
-				github.log "- Skipping ${version} change for ${target} as it is a base image and not a specific version."
-				continue
+	github.log "File changed:"
+	# shellcheck disable=SC2231
+	for lib in ${LIBRARIES}; do
+		for file in ${lib}/**/Dockerfile; do
+			github.log "- ${file}"
+			if [[ "${file}" == *"/.empty" ]] || [[ "${file}" == *"/Dockerfile" ]]; then
+				# Extract target and version from the file path
+				target=$(echo "${file}" | cut -d'/' -f1)
+				version=$(echo "${file}" | cut -d'/' -f2)
+				if [[ "${version}" == "base" ]]; then
+					github.log "- Skipping ${version} change for ${target} as it is a base image and not a specific version."
+					continue
+				fi
+				# Add to build matrix
+				echo "{\"target\":\"${target}\",\"version\":\"${version}\"}" >> "$BUILD_MATRIX_MANIFEST"
 			fi
-			# Add to build matrix
-			echo "{\"target\":\"${target}\",\"version\":\"${version}\"}" >> "$BUILD_MATRIX_MANIFEST"
-		fi
+		done
 	done
-done
+fi
 
 # Build JSON array and write to GITHUB_OUTPUT, quoting to prevent word splitting.
 if [ -z "${QUIET_LOGS:-}" ]; then
